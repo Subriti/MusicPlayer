6bdc5b61913c4d17f756ef436ec063a40b645060
Subriti <subritiaryal13@gmail.com>
2022-09-07 22:47:42 +0545

    Played a song

--- app/src/main/java/com/example/MusicPlayer/Controllers/AlbumsFragment.kt
+++ app/src/main/java/com/example/MusicPlayer/Controllers/AlbumsFragment.kt
@@ -1,19 +1,71 @@
 package com.example.MusicPlayer.Controllers
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.MusicPlayer.R
+import java.io.IOException
 
 class AlbumsFragment : Fragment() {
+
+    private lateinit var playBtn: Button
+    var mediaPlayer: MediaPlayer?= null
+    var isPlaying= false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_albums, container, false)
+        val v= inflater.inflate(R.layout.fragment_albums, container, false)
+
+        playBtn= v.findViewById(R.id.play_btn)
+
+        playBtn.setOnClickListener {
+            isPlaying = if (isPlaying) {
+                pauseAudio()
+                playBtn.text="Play Audio"
+                false
+            } else {
+                playAudio()
+                playBtn.text="Pause Audio"
+                true
+            }
+        }
+        return v
     }
+
+    private fun playAudio() {
+        //val audioURL= "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"
+        val audioURL= "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3"
+        mediaPlayer= MediaPlayer()
+        mediaPlayer!!.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        try{
+            mediaPlayer!!.setDataSource(audioURL)
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.isLooping=false
+            mediaPlayer!!.start()
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+        Toast.makeText(context, "Audio started playing", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun pauseAudio() {
+        if (mediaPlayer!=null && mediaPlayer!!.isPlaying){
+            mediaPlayer!!.stop()
+            mediaPlayer!!.reset()
+            mediaPlayer!!.release()
+            Toast.makeText(context,"Audio is paused", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(context,"Audio has not started playing", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
--- app/src/main/java/com/example/MusicPlayer/Controllers/SongsFragment.kt
+++ app/src/main/java/com/example/MusicPlayer/Controllers/SongsFragment.kt
@@ -1,10 +1,13 @@
 package com.example.MusicPlayer.Controllers
 
+import android.media.AudioManager
+import android.media.Image
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,10 +15,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.MusicPlayer.Adapters.SongsAdapter
 import com.example.MusicPlayer.R
 import com.example.MusicPlayer.Services.SongDatasource
-import kotlin.reflect.typeOf
 
 
 class SongsFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,16 +31,59 @@ class SongsFragment : Fragment() {
         val recyclerView = v.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(context)
 
+        val image= v.findViewById<ImageView>(R.id.song_image)
+
+      /*  playIB = findViewById(R.id.idIBPlay)
+        pauseIB = findViewById(R.id.idIBPause)*/
+
+        // on below line we are
+        // initializing our media player
+       /* val mediaPlayer = MediaPlayer()
+
+        recyclerView.setOnClickListener {
+
+            // on below line we are creating a variable for our audio url
+            var audioUrl = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+
+            // on below line we are setting audio stream
+            // type as stream music on below line.
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+
+            // on below line we are running a try
+            // and catch block for our media player.
+            try {
+                // on below line we are setting audio
+                // source as audio url on below line.
+                mediaPlayer.setDataSource(audioUrl)
+
+                // on below line we are
+                // preparing our media player.
+                mediaPlayer.prepare()
+
+                // on below line we are
+                // starting our media player.
+                mediaPlayer.start()
+
+            } catch (e: Exception) {
+
+                // on below line we are handling our exception.
+                e.printStackTrace()
+            }
+            // on below line we are displaying a toast message as audio player.
+            Toast.makeText(context, "Audio started playing..", Toast.LENGTH_SHORT).show()
+
+        }*/
+
         var adapter = context?.let {
             SongsAdapter(it, songDataset) { songs ->
                 //got stringId
-                val name= songs.getName()
+                val name = songs.getName()
 
                 //gives string name
                 println(resources.getResourceEntryName(songs.getName()))
 
                 //converted it to string value
-                val resource= resources.getString(name)
+                val resource = resources.getString(name)
 
                 Toast.makeText(context, resource, Toast.LENGTH_SHORT).show()
 
@@ -58,9 +104,9 @@ class SongsFragment : Fragment() {
 
             }
         }
-
         recyclerView.adapter = adapter
 
         return v
     }
+
 }
